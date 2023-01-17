6b121f3ea6308b795453c6c3c52db26b374dba2d
BringMeTheIoannis <ivankuzmenkov@icloud.com>
2023-01-17 21:13:52 +0300

    created Departments request

--- BankApp/BankApp/Provider/Provider.swift
+++ BankApp/BankApp/Provider/Provider.swift
@@ -17,12 +17,26 @@ class Provider {
         provider.request(.atm) { result in
             switch result {
             case .success(let response):
-                guard let departments = try? response.mapArray(ATMModel.self)
+                guard let atm = try? response.mapArray(ATMModel.self)
                 else {
                     failure?()
                     return
                 }
-                sucess(departments)
+                sucess(atm)
+            case .failure(let failure):
+                print(failure.localizedDescription)
+            }
+        }
+    }
+    
+    func getDepartments(success: @escaping ([DepartmentModel]) -> Void, failure: (()->Void)?) {
+        provider.request(.departments) { result in
+            switch result {
+            case .success(let response):
+                guard let departments = try? response.mapArray(DepartmentModel.self) else { failure?()
+                    return
+                }
+                success(departments)
             case .failure(let failure):
                 print(failure.localizedDescription)
             }
--- BankApp/BankApp/ViewController.swift
+++ BankApp/BankApp/ViewController.swift
@@ -12,11 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Provider().getATM { result in
-            print("Результат \(result[0].work_time)")
-        } failure: {
-            print("failure")
-        }
+        
     }
 }
 
