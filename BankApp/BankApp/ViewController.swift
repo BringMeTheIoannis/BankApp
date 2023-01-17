b2d11d495204226e0734649ff26280be12e76416
BringMeTheIoannis <ivankuzmenkov@icloud.com>
2023-01-17 19:21:48 +0300

    initial commit

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Ivan Kuzmenkov on 17.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
