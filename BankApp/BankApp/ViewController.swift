4d7e372bc7de4b51a50a42f1cd20e33f4eb02bd0
BringMeTheIoannis <ivankuzmenkov@icloud.com>
2023-01-18 23:14:59 +0300

    connect and give data to collectionViews

--- BankApp/BankApp/ViewController.swift
+++ BankApp/BankApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     var arrayOfDepartments = [DepartmentModel]()
     var cities = [String]()
     var selectedCity = IndexPath()
+    var selectedType = IndexPath(item: 0, section: 0)
+    var typesOfDeps = TypeOfDepartment.allCases
 
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var cityCollection: UICollectionView!
@@ -23,15 +25,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         cityCollection.dataSource = self
         cityCollection.delegate = self
+        typeCollection.dataSource = self
+        typeCollection.delegate = self
         registerCells()
         initMap()
         getATMS()
         getDepartments()
+        typeCollection.contentInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
     }
     
     func registerCells() {
         let nib = UINib(nibName: CityCollectionViewCell.id, bundle: nil)
         cityCollection.register(nib, forCellWithReuseIdentifier: CityCollectionViewCell.id)
+        typeCollection.register(nib, forCellWithReuseIdentifier: CityCollectionViewCell.id)
     }
     
     func initMap() {
@@ -57,9 +63,9 @@ class ViewController: UIViewController {
         Provider().getDepartments { [weak self] departments in
             guard let self = self else { return }
             self.arrayOfDepartments = departments
-            departments.forEach { dep in
-                self.drawMarker(lat: dep.gps_x, lon: dep.gps_y, color: .red)
-            }
+//            departments.forEach { dep in
+//                self.drawMarker(lat: dep.gps_x, lon: dep.gps_y, color: .red)
+//            }
         } failure: {
             print("Failure")
         }
@@ -88,23 +94,62 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        cities.count
+        if collectionView == cityCollection {
+            return cities.count
+        }
+        if collectionView == typeCollection {
+            return typesOfDeps.count
+        }
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CityCollectionViewCell.id, for: indexPath)
-        guard let cell = cell as? CityCollectionViewCell else { return cell }
-        cell.isSelected = selectedCity == indexPath
-        cell.setup()
-        cell.label.text = cities[indexPath.row]
+        if collectionView == cityCollection {
+            guard let cell = cell as? CityCollectionViewCell else { return cell }
+            cell.isSelected = selectedCity == indexPath
+            cell.setup()
+            cell.label.text = cities[indexPath.row]
+            return cell
+        }
+        if collectionView == typeCollection {
+            guard let cell = cell as? CityCollectionViewCell else { return cell}
+            cell.isSelected = selectedType == indexPath
+            cell.setup()
+            cell.label.text = typesOfDeps[indexPath.row].rawValue
+            return cell
+        }
         return cell
     }
 }
 
 extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        selectedCity = indexPath
-        collectionView.reloadData()
+        if collectionView == cityCollection {
+            selectedCity = indexPath
+            cityCollection.reloadData()
+        }
+        if collectionView == typeCollection {
+            selectedType = indexPath
+            typeCollection.reloadData()
+        }
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == typeCollection {
+            let inset = 5.0
+            guard let screen = view.window?.windowScene?.screen else { return .zero}
+            let cellCount = Double(typesOfDeps.count)
+            let width = (screen.bounds.width - (inset * (cellCount + 3))) / cellCount
+            return CGSize(width: width, height: 47.0)
+        }
+        if collectionView == cityCollection {
+            return CGSize(width: 171, height: 47)
+        }
+        return CGSize()
     }
 }
